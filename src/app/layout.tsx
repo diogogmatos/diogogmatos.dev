5d93255ac9cdc1da082a4ae5f676e0bda40e6302
Diogo Matos <me@diogogmatos.dev>
2026-05-19 04:17:33 +0200

    feat: update favicon and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,58 +35,6 @@ export const metadata: Metadata = {
     "max-video-preview": -1,
     googleBot: "index, follow",
   },
-  applicationName: "Diogo Matos | Software Engineer",
-  appleWebApp: {
-    title: "Diogo Matos | Software Engineer",
-    statusBarStyle: "default",
-    capable: true,
-  },
-  icons: {
-    icon: [
-      {
-        url: "/favicon.ico",
-        type: "image/x-icon",
-      },
-      {
-        url: "/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-      {
-        url: "/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        url: "/safari-pinned-tab.svg",
-        color: "#7b7b7b",
-        rel: "mask-icon",
-      },
-    ],
-    shortcut: [
-      {
-        url: "/favicon.ico",
-        type: "image/x-icon",
-      },
-    ],
-    apple: [
-      {
-        url: "/apple-touch-icon.png",
-        sizes: "180x180",
-      },
-    ],
-  },
-  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
